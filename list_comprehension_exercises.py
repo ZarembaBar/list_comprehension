7db518078190f2fbed706a9cc50c9812e2e4e703
Bartek <zaremba.bar@gmail.com>
2023-02-10 19:04:07 +0100

    second function with test

--- list_comprehension_exercises.py
+++ list_comprehension_exercises.py
@@ -1,3 +1,12 @@
-def all_numbers_form_0ne_to_thousand_divisible_by_seven():
+def all_numbers_form_one_to_thousand_divisible_by_seven():
     original_list = list(range(1, 1001))
     return [number for number in original_list if number % 7 == 0]
+
+
+def all_numbers_form_one_to_thousand_with_three_in_them():
+    original_list = list(range(1, 1001))
+    return [number for number in original_list if "3" in str(number)]
+
+
+if __name__ == "__main__":
+    print(all_numbers_form_one_to_thousand_with_three_in_them())
