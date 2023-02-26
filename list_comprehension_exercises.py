2ec94bcbdcdbdd1810df43b6851838f7a9e06e73
Bartek <zaremba.bar@gmail.com>
2023-02-26 13:39:06 +0100

    black fotmatter used, all changes made

--- list_comprehension_exercises.py
+++ list_comprehension_exercises.py
@@ -1,19 +1,19 @@
 from typing import List
 
 
-def all_numbers_form_one_to_thousand_divisible_by_seven():
+def all_numbers_form_one_to_thousand_divisible_by_seven() -> List[int]:
     original_list = list(range(1, 1001))
     return [number for number in original_list if number % 7 == 0]
 
 
-def all_numbers_form_one_to_thousand_with_three_in_them():
+def all_numbers_form_one_to_thousand_with_three_in_them() -> List[int]:
     original_list = list(range(1, 1001))
     return [number for number in original_list if "3" in str(number)]
 
 
 def count_the_number_of_spaces_in_string(sentence: str) -> int:
-    return len([space for space in sentence if space == " "])
+    return len([character for character in sentence if character == " "])
 
 
-def list_all_consonants_in_sentence(sentence: str) -> List:
+def list_all_consonants_in_sentence(sentence: str) -> List[chr]:
     return list([letter for letter in sentence if letter not in "aeiouAEIOU "])
